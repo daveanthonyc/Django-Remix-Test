b007bbd0360adec54ee32bf2ce2f992851338f9a
daveanthonyc <daveanthonyc@gmail.com>
2025-05-19 16:05:05 +1000

    style(math_utils): renamed function and variables per review feedback

--- backend/math_utils.py
+++ backend/math_utils.py
@@ -1,2 +1,2 @@
-def sum(a, b):
-    return a + b
+def add(num1, num2):
+    return num1 + num2
--- backend/tests/test_math_utils.py
+++ backend/tests/test_math_utils.py
@@ -1,15 +1,15 @@
 import unittest
-from math_utils import sum
+from math_utils import add
 
 class TestSumFunction(unittest.TestCase):
     def test_add_positive_numbers(self):
-        self.assertEqual(sum(3,5), 8)
+        self.assertEqual(add(3,5), 8)
 
     def test_add_negative_numbers(self):
-        self.assertEqual(sum(-3,-4), -7)
+        self.assertEqual(add(-3,-4), -7)
         
     def test_add_positive_negative_numbers(self):
-        self.assertEqual(sum(-3,10), 7)
+        self.assertEqual(add(-3,10), 7)
 
 if __name__ == "__main__":
     unittest.main()
